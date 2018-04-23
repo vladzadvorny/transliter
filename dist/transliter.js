7dc0be969ff69ea79eb97941a37faf168fe21f3a
Vlad Zadvorny <vladzadvorny@icloud.com>
2018-04-23 07:42:48 +0300

    build

--- dist/transliter.js
+++ dist/transliter.js
@@ -176,6 +176,8 @@ module.exports = function(text, separator) {
   separator = separator || '-';
   text = transliter(text);
   return text
+    .trim()
+    .replace(/ +(?= )/g, '')
     .replace(/ /g, separator)
     .replace(/[^a-zA-Z0-9-_]/g, '')
     .toLowerCase();
